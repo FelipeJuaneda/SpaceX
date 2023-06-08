b0b8e40d50ef75f5aefb7634fdabd39773d4a0ba
Felipe Juaneda <felipejua9@gmail.com>
2023-06-08 08:35:48 -0300

    creando rama detalle

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -2,7 +2,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, IconButton, Stack } from "@mui/material";
+import { IconButton, Stack } from "@mui/material";
 import { AiOutlineStar } from "react-icons/ai";
 
 const CardList = ({ launch, rocket }) => {
@@ -31,58 +31,56 @@ const CardList = ({ launch, rocket }) => {
           "linear-gradient(0deg, rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05)), #121212",
       }}
     >
-      <CardActionArea>
-        {rocket?.flickr_images && (
-          <CardMedia
-            component="img"
-            height="140"
-            image={randomImage}
-            alt={randomImage}
-          />
-        )}
-        <CardContent>
-          <Stack height={120} direction="column" justifyContent="center">
-            <Typography
-              color="#FFFFFF"
-              gutterBottom
-              fontSize={"20px"}
-              fontStyle={"normal"}
-              fontWeight={"700"}
-              lineHeight={"32px"}
-              component="div"
-            >
-              {launch.name}
-            </Typography>
-            <Typography
-              sx={{
-                color: "#FFFFFF",
-                display: "-webkit-box",
-                WebkitLineClamp: "2",
-                WebkitBoxOrient: "vertical",
-                textOverflow: "ellipsis",
-                overflow: "hidden",
-              }}
-              variant="body2"
-              color="text.secondary"
-            >
-              {launch.details
-                ? launch.details
-                : "First orbital class rocket capable of reflight"}
-            </Typography>
-            <Stack
-              sx={{ color: "#ffffff78" }}
-              direction="row"
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Typography fontSize={"14px"}>{formattedDate}</Typography>
-              <IconButton sx={{ color: "#ffffff78" }}>
-                <AiOutlineStar fontSize={"20px"} />
-              </IconButton>
-            </Stack>
+      {rocket?.flickr_images && (
+        <CardMedia
+          component="img"
+          height="140"
+          image={randomImage}
+          alt={randomImage}
+        />
+      )}
+      <CardContent>
+        <Stack height={120} direction="column" justifyContent="center">
+          <Typography
+            color="#FFFFFF"
+            gutterBottom
+            fontSize={"20px"}
+            fontStyle={"normal"}
+            fontWeight={"700"}
+            lineHeight={"32px"}
+            component="div"
+          >
+            {launch.name}
+          </Typography>
+          <Typography
+            sx={{
+              color: "#FFFFFF",
+              display: "-webkit-box",
+              WebkitLineClamp: "2",
+              WebkitBoxOrient: "vertical",
+              textOverflow: "ellipsis",
+              overflow: "hidden",
+            }}
+            variant="body2"
+            color="text.secondary"
+          >
+            {launch.details
+              ? launch.details
+              : "First orbital class rocket capable of reflight"}
+          </Typography>
+          <Stack
+            sx={{ color: "#ffffff78" }}
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography fontSize={"14px"}>{formattedDate}</Typography>
+            <IconButton sx={{ color: "#ffffff78" }}>
+              <AiOutlineStar fontSize={"20px"} />
+            </IconButton>
           </Stack>
-        </CardContent>
-      </CardActionArea>
+        </Stack>
+      </CardContent>
     </Card>
   );
 };
